e16733366085b7286f8dad0b2688a78c982e5cc6
antipol <trine_54@hotmail.com>
2019-05-09 09:21:42 +0200

    uploading files for alternative portfolio

---
+++ script.js
@@ -0,0 +1,56 @@
+function openAbout() {
+  document.getElementById("about").classList.toggle("content-show");
+  document.getElementById("borderAbout").classList.toggle("button-toggle");
+}
+
+function openProjects() {
+  document.getElementById("projects").classList.toggle("content-show")
+  document.getElementById("borderProjects").classList.toggle("button-toggle");
+}
+
+function openContact() {
+  document.getElementById("contact").classList.toggle("content-show")
+  document.getElementById("borderContact").classList.toggle("button-toggle");
+  document.getElementById("waterToggle").classList.toggle("water-show");
+}
+
+// setting slideIndex to 1 = first image
+var slideIndex = 1;
+// show first image
+showDivs(slideIndex);
+
+// when buttons are clicked, show next (add 1) or previous img (subtract 1)
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+// the indicator will take user to connnected img when clicked
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  var indicator = document.getElementsByClassName("indicator");
+  // if current img is the last one, start slides over on click to right
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  // if current img is the first, show last img when left button is clicked
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  // to always hide the other images
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  // remove fill class (white bg) from all indicators
+  for (i = 0; i < indicator.length; i++) {
+    indicator[i].className = indicator[i].className.replace(" fill", "");
+  }
+  // to show current image only
+  x[slideIndex-1].style.display = "block";
+  // to fill the current indicator
+  indicator[slideIndex-1].className += " fill";
+}
